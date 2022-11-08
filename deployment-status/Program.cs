36819e7cf74baee575cf6f165583778e14a416af
Chris <crinehart22@yahoo.com>
2022-11-07 19:05:38 -0500

    init

---
+++ deployment-status/Program.cs
@@ -0,0 +1,7 @@
+using deployment_status;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
